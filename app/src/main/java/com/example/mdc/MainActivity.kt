aab8f9025dcc4b01f340eef44180800e08993926
Meet K. Parmar <meetk.parmar@Meets-MacBook-Air.local>
2020-12-08 14:56:51 +0530

    Add button layout

--- app/src/main/java/com/example/mdc/MainActivity.kt
+++ app/src/main/java/com/example/mdc/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.mdc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
